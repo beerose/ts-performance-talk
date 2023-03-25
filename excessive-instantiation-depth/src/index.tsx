7e3b8a836522d382ee83f57e2779132882ab345d
beerose <alexsandra.sikora@gmail.com>
2023-03-25 14:58:16 +0100

    Add excessive instantation depth example

---
+++ excessive-instantiation-depth/src/index.tsx
@@ -0,0 +1,25 @@
+// ref: https://github.com/microsoft/TypeScript/issues/46783
+
+class Col<TParams> {
+  public withRequiredPrimitiveParam<TName extends string>(_name: TName) {
+    return undefined! as Col<TParams & { readonly [K in TName]: any }>;
+    // return undefined! as Col<TParams & Record<TName, any>>;
+  }
+}
+
+const _ = new Col()
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value")
+  .withRequiredPrimitiveParam("value");
+// .withRequiredPrimitiveParam("value"); // Hits stack limit
